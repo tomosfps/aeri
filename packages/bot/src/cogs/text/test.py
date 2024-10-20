ed481a2d1335dd9915ad270f1d2e4300fc389173
tomos <oerushia@gmail.com>
2024-10-20 10:22:16 +0100

    feat: cog support

--- packages/bot/src/cogs/text/test.py
+++ packages/bot/src/cogs/text/test.py
@@ -1,11 +1,13 @@
 from nextcord import Interaction
 from nextcord.ext import commands
 
-
-class General(commands.Cog):
+class Test(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     @commands.command(name='ping')
     async def ping(self, ctx: Interaction):
         await ctx.send('Pong!')
+
+def setup(client):
+    client.add_cog(Test(client))
