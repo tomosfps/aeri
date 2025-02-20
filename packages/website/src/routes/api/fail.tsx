453fcebf1025643875bad6d39a9ff4c55f0bc75f
Tomos <oerushia@gmail.com>
2025-02-20 23:46:50 +0000

    feat(website): remove sad image asset, update Fail and Success components with new messaging and layout

--- packages/website/src/routes/api/fail.tsx
+++ packages/website/src/routes/api/fail.tsx
@@ -1,11 +1,10 @@
-import sad from "@/assets/sad.png";
-
 export default function Fail() {
     return (
         <>
-            <main className="w-dvw h-dvh flex flex-col items-center justify-center text-center">
-                <img src={sad} alt="Sad Face" width={"200"} height={"200"} />
-                <h1 className="text-2xl 1920p:text-4xl 2k:text-6xl text-wrap pb-40">An error occured trying to link your account.</h1>
+            <main className="w-dvw h-dvh flex flex-col items-center justify-center text-center space-y-6">
+                <p className="text-6xl">:(</p>
+                <h1 className="text-2xl 1920p:text-4xl 2k:text-6xl text-wrap">An error occured trying to link your account.</h1>
+                <p className="text-gray-600 dark:text-gray-300/50 pb-40">Either try again, or contact the owners on Discord.</p>
             </main>
         </>
     );
