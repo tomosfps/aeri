bdc8747ffad62dcf21ada230bfcbdc7a88120ddd
Tomos <oerushia@gmail.com>
2025-02-22 17:27:33 +0000

    Update text colors and styles in login success and failure pages

--- packages/website/src/routes/login/fail.tsx
+++ packages/website/src/routes/login/fail.tsx
@@ -2,9 +2,9 @@ export default function Fail() {
     return (
         <>
             <main className="w-dvw h-dvh flex flex-col items-center justify-center text-center space-y-6">
-                <p className="text-6xl">:(</p>
-                <h1 className="text-2xl 1920p:text-4xl 2k:text-6xl text-wrap">An error occured trying to link your account.</h1>
-                <p className="text-gray-600 pb-40">Either try again, or contact the owners on Discord.</p>
+                <p className="text-6xl text-ctext-light">:(</p>
+                <h1 className="text-2xl 1920p:text-4xl 2k:text-6xl text-wrap text-ctext-light">An error occured trying to link your account.</h1>
+                <p className="text-gray-500/90 pb-40">Either try again, or contact the owners on Discord.</p>
             </main>
         </>
     );
