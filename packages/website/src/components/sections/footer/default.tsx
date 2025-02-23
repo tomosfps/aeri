af0e1754ffffd043ce188f06801ca7a040de418b
Tomos <oerushia@gmail.com>
2025-02-23 05:31:27 +0000

    Remove footer

--- packages/website/src/components/sections/footer/default.tsx
+++ packages/website/src/components/sections/footer/default.tsx
@@ -8,11 +8,6 @@ export default function FooterSection() {
           <FooterBottom className="mt-0 flex flex-col items-center gap-4 sm:flex-col md:flex-row text-ctext-light">
             <div className="flex flex-row items-center gap-4">
               <p>© 2025 Aeri. All rights reserved</p>
-              </div>
-            <div className="flex items-center gap-4">
-              <a className="hover:text-cprimary-light ">Sign In</a> <a className="hover:text-cprimary-light">Sign Out</a>|
-              <a href="privacy" className="hover:text-cprimary-light">Privacy Policy</a>
-              <a href="terms" className="hover:text-cprimary-light">Terms of Service</a>
             </div>
           </FooterBottom>
         </Footer>
