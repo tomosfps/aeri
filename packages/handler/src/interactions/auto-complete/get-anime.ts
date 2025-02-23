758f6c85c137435f94d5cccbe3df58f48eda7f20
Tomos <oerushia@gmail.com>
2025-02-23 16:53:56 +0000

    fix(get-anime) add missing comma for correct object formatting

--- packages/handler/src/interactions/auto-complete/get-anime.ts
+++ packages/handler/src/interactions/auto-complete/get-anime.ts
@@ -4,7 +4,7 @@ import type { AutoCompleteCommand } from "../../services/commands.js";
 const logger = new Logger();
 const results = Array.from({ length: 24 }, (_, i) => ({
     name: `id_${i}`,
-    value: i
+    value: i,
 }));
 
 export const interaction: AutoCompleteCommand = {
